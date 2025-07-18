96979aa342caa40c1dcb872ddba2a53bb9e1a1bb
Kyle <kyclone@sbcglobal.net>
2025-07-18 14:37:01 -0700

    robo stroker

--- VECTOOOOORRRRRRRRR.py
+++ VECTOOOOORRRRRRRRR.py
@@ -3,7 +3,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.interpolate import make_interp_spline
+from scipy.interpolate import make_interp_spline,interp1d
 
 # SET STATIC PARAMETERS
 LAP_ALTITUDE = 200 #ft
@@ -90,6 +90,38 @@ def calculate_thrust(vel_mph:float, thrust_interp) -> float:
     Returns the thrust in ounces.
     """
     return thrust_interp(vel_mph)
+
+def xflr_dat(filename,name,value):
+    # --- read the polar -------------------------------------------------
+    df = pd.read_csv(
+    filename,
+    comment="#",          # skip every line that starts with ‘#’
+    skipinitialspace=True # drop spaces that follow each comma
+    )
+    # clean the headers
+    df.columns = df.columns.str.strip()   # remove stray spaces
+    # --- convert strings to numbers (just in case) ----------------------
+    df = df.apply(pd.to_numeric, errors="coerce")
+
+    cd_of_cl = interp1d(df["CL"], df["CD"], kind='cubic')
+    alpha_of_cl = interp1d(df["CL"], df["alpha"], kind='cubic')
+    cl_of_cd = interp1d(df["CD"], df["CL"], kind='cubic')
+    alpha_of_cd = interp1d(df["CD"], df["alpha"], kind='cubic')
+    cd_of_alpha = interp1d(df["alpha"], df["CD"], kind='cubic')
+    cl_of_alpha = interp1d(df["alpha"], df["CL"], kind='cubic')
+
+    if name == "CL" or "cl" or "Cl" or "cL":
+        CD = cd_of_cl(value)
+        alpha = alpha_of_cl(value)
+        return CD,alpha
+    if name == "CD" or "cd" or 'Cd' or "cD":
+        CL = cl_of_cd(value)
+        alpha = alpha_of_cd(value)
+        return CL, alpha
+    if name == "alpha" or "Alpha":
+        CL = cl_of_alpha(value)
+        CD = cd_of_alpha(value)
+        return CL,CD
 
 # Running the file:
 if __name__ == '__main__':
--- XFLRdat.py
+++ XFLRdat.py
@@ -2,28 +2,38 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
-# --- read the polar -------------------------------------------------
-df = pd.read_csv(
-    "Lark_8lb_VLMvisc.csv",
+def xflr_dat(filename,name,value):
+    # --- read the polar -------------------------------------------------
+    df = pd.read_csv(
+    filename,
     comment="#",          # skip every line that starts with ‘#’
     skipinitialspace=True # drop spaces that follow each comma
-)
+    )
+    # clean the headers
+    df.columns = df.columns.str.strip()   # remove stray spaces
+    # --- convert strings to numbers (just in case) ----------------------
+    df = df.apply(pd.to_numeric, errors="coerce")
 
-# clean the headers
-df.columns = df.columns.str.strip()   # remove stray spaces
+    cd_of_cl = interp1d(df["CL"], df["CD"], kind='cubic')
+    alpha_of_cl = interp1d(df["CL"], df["alpha"], kind='cubic')
+    cl_of_cd = interp1d(df["CD"], df["CL"], kind='cubic')
+    alpha_of_cd = interp1d(df["CD"], df["alpha"], kind='cubic')
+    cd_of_alpha = interp1d(df["alpha"], df["CD"], kind='cubic')
+    cl_of_alpha = interp1d(df["alpha"], df["CL"], kind='cubic')
 
-# --- convert strings to numbers (just in case) ----------------------
-df = df.apply(pd.to_numeric, errors="coerce")
+    if name == "CL" or "cl" or "Cl" or "cL":
+        CD = cd_of_cl(value)
+        alpha = alpha_of_cl(value)
+        return CD,alpha
+    if name == "CD" or "cd" or 'Cd' or "cD":
+        CL = cl_of_cd(value)
+        alpha = alpha_of_cd(value)
+        return CL, alpha
+    if name == "alpha" or "Alpha":
+        CL = cl_of_alpha(value)
+        CD = cd_of_alpha(value)
+        return CL,CD
 
-# --- use the data ---------------------------------------------------
-alpha = df["alpha"]
-cl    = df["CL"]
-cd    = df["CD"]
 
-cd_of_cl = interp1d(df["CL"], df["CD"], kind='cubic')
-alpha_of_cl = interp1d(df["CL"], df["alpha"], kind='cubic')
-
-cl_query = 0.3775
-cd_value = cd_of_cl(cl_query)
-alpha_value = alpha_of_cl(cl_query)
-print(f"Interpolated AOA and CD at CL={cl_query:.2f} are {alpha_value:.5f} and {cd_value:.5f}")
+results = xflr_dat("Lark_8lb_VLMvisc.csv","Cl",0.3778)
+print(results)
