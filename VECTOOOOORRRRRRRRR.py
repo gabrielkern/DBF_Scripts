04e852cf61fee36031e9401dd21bfd2bd27e0306
gabrielkern <gabrieldkern@gmail.com>
2025-07-18 15:07:40 -0700

    WE COOKING OH YEAHHHHHHHHHHH

--- VECTOOOOORRRRRRRRR.py
+++ VECTOOOOORRRRRRRRR.py
@@ -11,9 +11,17 @@ CLIMB_ANGLE = 30 #deg
 WING_AREA = 4.44 #ft^2
 WEIGHT = 8.0 #lbs
 COEFF_FRICTION = 0.04 #unitless
+GRAVITY = 32.174 #ft/s^2
+RHO = 0.0023769 #slugs/ft^3 at sea level
+DT = 0.05 #seconds
+BATTERY_CAPACITY = 3300 #mAh
+BATTERY_CELLS = 4 #number of cells in series
 
 MOTOCALC_FILEPATH = '201_14x12.csv'
-XFLR5_FILEPATH = ''
+XFLR5_FILEPATH = 'Lark_8lb_VLMvisc.csv'
+
+# Calculate remaining parameters
+Mass = WEIGHT / GRAVITY # slugs
 
 def imported_data(csv_filepath:str) -> pd.DataFrame:
     """
@@ -70,26 +78,27 @@ def create_thrust_interpolator(prop_data: pd.DataFrame):
         sorted_data['thrust_oz']
     )
 
-def calculate_charge(current_charge:float,vel_mph:float, dt:float, batt_interp) -> float:
+def calculate_charge(current_charge:float,vel_fps:float, dt:float, batt_interp) -> float:
     """
     Calculates the new battery charge given usage at speed vel_mph and the data
     interpolated and passed through batt_interp. Inputs the old charge, the
     velocity in mph, the timestep in seconds, and the batt_interp function.
     Returns the new charge in milliamp-hours.
     """
-
+    vel_mph = vel_fps * 0.681818 # convert ft/s to mph
     amps = batt_interp(vel_mph)
     charge_used = amps * (dt / 3600) * 1000  # Convert to mAh of charge used
     new_charge = current_charge - charge_used
-    return max(new_charge, 0)  # Ensure change in charge isn't negative
+    return max(new_charge, 0)  # Ensure change in charge isn't negative, also in mAh
 
-def calculate_thrust(vel_mph:float, thrust_interp) -> float:
+def calculate_thrust(vel_fps:float, thrust_interp) -> float:
     """
     Calculates the thrust at a given velocity using the thrust_interp function.
     Inputs the velocity in mph and the thrust_interp function.
     Returns the thrust in ounces.
     """
-    return thrust_interp(vel_mph)
+    vel_mph = vel_fps * 0.681818 # convert ft/s to mph
+    return ((thrust_interp(vel_mph)) * 0.0625) # convert oz to lbs
 
 def xflr_dat(filename,name,value):
     # --- read the polar -------------------------------------------------
@@ -126,10 +135,53 @@ def xflr_dat(filename,name,value):
 # Running the file:
 if __name__ == '__main__':
     
+    """
+    To get thrust, call calculate_thrust(vel_fps, thrust_interp), where
+    vel_fps is the velocity in feet per second and thrust_interp is a constant.
+
+    To get battery charge, call calculate_charge(current_charge, vel_fps, dt, batt_interp),
+    where current_charge is the current battery charge in mAh, vel_fps is the
+    velocity in feet per second, dt is the timestep in seconds, and batt_interp
+    is a constant.
+
+    
+    """
+
     # First get all the constant parameters into a structured dictionary
+    constants = {
+        'altitude': LAP_ALTITUDE,
+        'theta': CLIMB_ANGLE,
+        'S': WING_AREA,
+        'W': WEIGHT,
+        'Cf': COEFF_FRICTION,
+        'm': Mass,
+        'g': GRAVITY,
+        'rho': RHO,
+        'dt': DT, 
+        'battery_capacity': BATTERY_CAPACITY,
+        'battery_cells': BATTERY_CELLS
+    }
 
     # Second create the interpolators with the imported MotoCalc data to be parsed
     imported_dataframe = imported_data(MOTOCALC_FILEPATH)
     batt_interp = create_batt_interpolator(imported_dataframe)
     thrust_interp = create_thrust_interpolator(imported_dataframe)
-    print("Interpolators created successfully.")
+    print(f"Interpolators created successfully. {constants['battery_capacity']}")
+
+    # Third set up initial state as well as the state directory
+    state = {
+        'velocity': 0.0, #ft/s
+        'position': 0.0, #ft
+        'acceleration': 0.0, #ft/s^2
+        'battery_charge': constants['battery_capacity'], #mAh
+        'time': 0.0, #seconds
+        'turn_angle': 0.0, #degrees
+        'thrust': 0.0, #lbs
+        'Cl': 0.0,
+        'Cd': 0.0,
+        'alpha': 0.0,
+        'lift': 0.0, #lbs
+        'drag': 0.0, #lbs
+        'F_long': 0.0, #lbs
+        'F_lat': 0.0 #lbs
+    }
