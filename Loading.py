875501d0ae03e03b76278c624d08ddb18c9ce63c
Kyle <kyclone@sbcglobal.net>
2025-08-14 13:57:18 -0700

    plower

--- Loading.py
+++ Loading.py
@@ -4,29 +4,37 @@ import matplotlib.pyplot as plt
 
 ########################################################################
 
+Acc = 606 # number of points do not touch causes problems with moment
+
 #Fuelselage
-Wb = 5 # body weight lbs
-n = 9  # g load
-placedlocations = [ 0.01 , 0.2 , 0.6] # location of motor,esc,battery
-placedweights = [1 , 1 , 1] # weights corresponding to above
+Wb = 6 # body weight lbs
+n = 10  # g load
+placedlocations = [0.1,0.3] # location of motor,esc,battery
+placedweights = [1,1] # weights corresponding to above
 # x=0 at nose
-cg = 0.5 # location of cg
+cg = 0.7 # location of cg
 w = 0.5 # quarter chord positions
 L = 2 # total length
-Acc = 100 # number of points
-Bw = 5 # inches this is body width to zero out the lift
 
 #Wing
-Ww = 3 # wing weight in pounds
-taper = np.array([0.7,0.6,0.8,1]) # taper ratio
-Cr = 8 /12 # root chord inches
+Ww = 2 # wing weight in pounds
+taper = np.array([1]) # taper ratio
+Cr = 10.66 /12 # root chord inches #############
 a0 = 2*np.pi #section lift coefficient
 b_input= 60  /12 # span inches
 B_width = 5  /12# body width in inches
-term = 100 # number of terms
+term = 50 # number of terms
 alphain = 3 # angle of attack
 alphanolift = -1.6
 rho = 0.0023769  # slugs/ft³
+
+#Spar
+h = 0.70 # height of the box beam in inches
+b = 0.70 # wisth of the box beam
+capt = 1/8 #thickness of wood used for bending cap
+Es = 63.81  # elastic modulus of wood selected: balsa shear web
+Eb = 1044.271 # basswood elastic modulus bending caps
+FS = 2 # factor of safety
 
 ########################################################################
 
@@ -61,7 +69,7 @@ shear = np.zeros_like(x_vals)
 for j in range(len(x_vals)): 
     for k in range(len(locations)):
         if x_vals[j] >= locations[k]:
-            shear[j] = shear[j]+weights[k]*n
+            shear[j] += weights[k]*n
 
 
 # moment calcs
@@ -74,50 +82,30 @@ for j in range(len(moment)-1):
 
 
 # Plot shear and moment diagrams
-
-plt.figure(figsize=(10, 4))
-plt.plot(x_vals, -shear, label='Shear Force', color='royalblue')
-plt.plot(x_vals, moment, label='Moment', color='hotpink')
-plt.axhline(0, color='k', linestyle='--', linewidth=1)
-plt.title("Fuselage Shear Force and Moment Diagram")
-plt.xlabel("Fuselage Length (ft)")
-plt.ylabel("Shear Force (lbs) or Moment (lb*in)")
-plt.grid(True)
-plt.legend()
-plt.tight_layout()
-plt.show()
+fig, axs = plt.subplots(2, 1, figsize=(10, 8))  # 2 rows, 1 column of subplots
+axs[0].plot(x_vals, -shear, label='Shear Force', color='royalblue')
+axs[0].plot(x_vals, moment, label='Moment', color='hotpink')
+axs[0].axhline(0, color='k', linestyle='--', linewidth=1)
+axs[0].set_title("Fuselage Shear Force and Moment Diagram")
+axs[0].set_xlabel("Fuselage Length (ft)")
+axs[0].set_ylabel("Shear Force (lbs) or Moment (lbft)")
+axs[0].grid(True)
+axs[0].legend()
 
 
 ########################################################################
 
-# Visualize Wing Shape
-
 b = b_input - B_width  ####### span accounting for body being in the middle in feet
-plt.figure(figsize=(10,4))
 y = np.linspace(0,b/2,Acc)
 for j in taper:
     C_y = Cr * ( 1- 2*y/b * (1-j))
-    
-    plt.plot(y,C_y/2, label='Right Wing Dist', color='royalblue')
-    plt.plot(-y,C_y/2, label='Left Wing Dist', color='hotpink')
-    plt.plot(y,-C_y/2, label='Right Wing Dist', color='royalblue')
-    plt.plot(-y,-C_y/2, label='Left Wing Dist', color='hotpink')
-    plt.plot([b/2, b/2], [-C_y[-1]/2, C_y[-1]/2], 'k-')
-    plt.plot([-b/2, -b/2], [-C_y[-1]/2, C_y[-1]/2], 'k-')
-
-plt.axhline(0, color='k', linestyle='--', linewidth=1)
-plt.title("Trapezoidal Wing")
-plt.xlabel("Distance from Center (in)")
-plt.grid(True)
-plt.tight_layout()
-plt.show()
-
 
 ########################################################################
 
 # Load on wing Span Wise
 
 TotalLoad = (WT+Ww) * n # finds total load of weight at max G value
+
 
 
 # Gabes Code
@@ -224,7 +212,7 @@ for current_taper_ratio in taper:
         CL_wing, CDi_wing = calculate_finite_wing_coeffs(An_coeffs, n_indices_vals, AspectR)
 
 
-        print(CDi_wing)
+        #print(CDi_wing)
         y_s_plot_data, Cl_local_data = get_spanwise_Cl_data(
                                              An_coeffs, n_indices_vals,
                                              Cr, current_taper_ratio, semi_span_s,
@@ -237,10 +225,10 @@ for current_taper_ratio in taper:
             Intboy[r] = C_y[r] * Cl_local_data[r]
         #print(Intboy)
 
-
+        S = ((Cr + Cr*current_taper_ratio)*b/2)
         U_needed = ((WT+Ww)*n*2 / rho / np.trapz(Intboy,x=full_y) )**0.5  # finds U_inf in ft/s  np.trapz(Intboy,x=full_y)
         #U_neccesary.append(Uneeded)
-        print(U_needed/1.467) #prints in mph
+        #print(U_needed/1.467) #prints in mph
 
         # solve for shear from lift
         LiftForce = np.zeros_like(C_y)
@@ -250,12 +238,15 @@ for current_taper_ratio in taper:
             LiftForce[index] = 1/2 * rho * U_needed**2 * (C_y[index] * Cl_local_data[index]+C_y[index+1] * Cl_local_data[index+1])/2 * abs(full_y[index]-full_y[index+1])
         #print(LiftForce)
         # moving all points over
+        
         for g in range(len(C_y)):
             if full_y[g]<= 0:
                 full_y[g] -= B_width/2
             if full_y[g] > 0:
                 full_y[g] += B_width/2
+        
 
+        
         # creating the body section of the plot
         fill = np.linspace(-B_width/2,B_width/2,101) 
         full_y = np.insert(full_y,Acc,fill) #inserts this into the body
@@ -263,8 +254,8 @@ for current_taper_ratio in taper:
         LiftForce = np.insert(LiftForce,Acc,np.zeros_like(fill))  #inserts zeros for load into the Lift force so weights can be added
         for t in range(len(LiftForce)-2):
                 if LiftForce[t+1] == 0:
-                    LiftForce[t+1] = -(WT+Ww)/102*n #distributes wing weight and whole body weight along the contact
-
+                    LiftForce[t+1] = -(WT+Ww)/101*n #distributes wing weight and whole body weight along the contact
+        
         for j in range(len(full_y)): 
              wing +=LiftForce[j]
              wingshear.append(wing)
@@ -273,34 +264,59 @@ for current_taper_ratio in taper:
         wingmoment =np.zeros_like(full_y)
         val = 0
         for j in range(len(wingshear)-1):
-            val += 0.5 * b/(2*Acc+101) * (wingshear[j] + wingshear[j+1])  # trapezoidal integration
-            wingmoment[j+1] = val  # assign to the next point
-                
-        plt.plot(full_y,wingshear, label = f"$\\ Shear \\ lambda = {current_taper_ratio:.1f}$")
-        plt.plot(full_y,wingmoment, label =f"$\\ Moment \\ lambda = {current_taper_ratio:.1f}$")
-        plt.legend()
-        plt.legend(loc='best')
-        plt.grid(True)
-        plt.xlabel("Distance from the Center")
-        plt.ylabel("Shear (lb) and Moment (lbft)")
-        plt.tight_layout()
-        #print(np.sum(LiftForce))
-        spanwise_data_all_tapers.append((y_s_plot_data, Cl_local_data , current_taper_ratio))
+            #if j<= Acc:
+                val += 0.5 * b/(Acc*2) * (wingshear[j] + wingshear[j+1])  # trapezoidal integration
+            #elif j <= Acc+102:
+                #val += 0.5 * B_width/(101) * (wingshear[j] + wingshear[j+1])
+            #elif j > Acc+102:
+                #val += 0.5 * b/2/(Acc) * (wingshear[j] + wingshear[j+1])
 
-plt.show()
+                wingmoment[j+1] = val  # assign to the next point
+        
 
-'''
-plt.figure(figsize=(10, 6))
-for y_s_data, Cl_data, lambda_val in spanwise_data_all_tapers:
-        if y_s_data.size > 1:
-            plt.plot(y_s_data, Cl_data, label=f"$\\lambda = {lambda_val:.1f}$")
 
-plt.xlabel("Normalized Spanwise Location ($y/s$)")
-plt.ylabel("Local Section Lift ($C_l(y)$)")
-plt.title("Spanwise Local Lift Distribution")
-plt.legend(loc='best')
-plt.grid(True)
-plt.xlim([-1, 1])
+
+
+
+
+        #########################################################################################################################################
+        #########################################################################################################################################
+        #########################################################################################################################################
+        #########################################################################################################################################
+        #########################################################################################################################################
+
+        # finding max bending and shear and finding cross sections of wood to use
+
+        wing_max_shear = np.array([np.max(np.abs(wingshear)), wingmoment[np.argmax(np.abs(wingshear))], full_y[np.argmax(np.abs(wingshear))]])
+        wing_max_moment = np.array([np.max(np.abs(wingmoment)), wingshear[np.argmax(np.abs(wingmoment))], full_y[np.argmax(np.abs(wingmoment))]])
+        print(wing_max_shear)
+        print(wing_max_moment)
+
+
+        
+
+
+        #########################################################################################################################################
+        #########################################################################################################################################
+        #########################################################################################################################################
+        #########################################################################################################################################
+        #########################################################################################################################################    
+
+
+
+
+
+
+        axs[1].plot(full_y, wingshear, label=f"$\\ Shear \\ lambda = {current_taper_ratio:.1f}$")
+        axs[1].plot(full_y, wingmoment, label=f"$\\ Moment \\ lambda = {current_taper_ratio:.1f}$")
+        axs[1].legend(loc='best')
+        axs[1].grid(True)
+        axs[1].set_xlabel("Distance from the Center (ft)")
+        axs[1].set_ylabel("Shear (lb) and Moment (lbft)")
+
 plt.tight_layout()
 plt.show()
-'''
+
+
+
+
