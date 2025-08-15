982e5375fd6f40b4c48736455a6635a00056cc41
Kyle <kyclone@sbcglobal.net>
2025-08-14 19:48:33 -0700

    de

--- climbfunc.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from scipy.interpolate import make_interp_spline,interp1d
-
-def climb(thrust_interp,state,constants,bat_interp,)
